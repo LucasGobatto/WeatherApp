3859678f3398757be2d657c1e0e0e066e99e4f70
LucasGobatto <lucas.bisaio@live.com>
2020-10-09 23:40:14 -0300

    store the climate details in cache

--- src/hooks/WeatherHook/index.tsx
+++ src/hooks/WeatherHook/index.tsx
@@ -2,12 +2,14 @@ import React, { createContext, useCallback, useState, useContext } from 'react';
 import { climateCityDetails } from '../../services/api.requests';
 import { ClimateDetails } from '../../services/services.interface';
 import toFahrenheit from '../../utils/functions/toFahrenheit';
+import { tempUnitToken } from '../../store';
 
 interface WeatherContextData {
 	climate: ClimateDetails | null;
 	tempUnit: 'celcius' | 'fahrenheit';
 	getClimate(woeid: number): void;
 	changeTempUnit(unit: WeatherContextData['tempUnit']): void;
+	setClimate: React.Dispatch<React.SetStateAction<ClimateDetails | null>>;
 }
 
 const WeatherContext = createContext<WeatherContextData>({} as WeatherContextData);
@@ -15,7 +17,7 @@ const WeatherContext = createContext<WeatherContextData>({} as WeatherContextDat
 export const WeatherProvider: React.FC = ({ children }) => {
 	const [climate, setClimate] = useState<ClimateDetails | null>(null);
 	const [tempUnit, setTempUnit] = useState<WeatherContextData['tempUnit']>(() => {
-		const savedUnit = localStorage.getItem('@WeatherApp:tempUnit') as WeatherContextData['tempUnit'];
+		const savedUnit = localStorage.getItem(tempUnitToken) as WeatherContextData['tempUnit'];
 
 		if (savedUnit) {
 			return savedUnit;
@@ -43,7 +45,7 @@ export const WeatherProvider: React.FC = ({ children }) => {
 	const changeTempUnit = useCallback((unit: WeatherContextData['tempUnit']) => {
 		// TODO: Save selected temp unit to localStorage
 
-		localStorage.setItem('@WeatherApp:tempUnit', unit);
+		localStorage.setItem(tempUnitToken, unit);
 
 		setTempUnit(unit);
 	}, []);
@@ -51,14 +53,14 @@ export const WeatherProvider: React.FC = ({ children }) => {
 	const getClimate = useCallback(
 		async (woeid: number) => {
 			const result = await climateCityDetails(woeid);
-
+			console.log('The requistion is done! The result is:', result);
 			setClimate(addFahrenheitTemps(result));
 		},
 		[addFahrenheitTemps],
 	);
 
 	return (
-		<WeatherContext.Provider value={{ climate, tempUnit, getClimate, changeTempUnit }}>
+		<WeatherContext.Provider value={{ climate, tempUnit, getClimate, setClimate, changeTempUnit }}>
 			{children}
 		</WeatherContext.Provider>
 	);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,9 +4,11 @@ import { useWeather } from '../hooks/WeatherHook';
 import { useLoading } from '../hooks/LoadingHook';
 import { requestByLattAndLong } from '../services/api.requests';
 import { Container } from './styles';
+import { ClimateDetails } from '../services/services.interface';
+import { climateDetailsToken } from '../store';
 
 const Home: React.FC = ({ children }) => {
-	const { climate, getClimate } = useWeather();
+	const { climate, getClimate, setClimate } = useWeather();
 	const { setLoading } = useLoading();
 
 	React.useEffect(() => {
@@ -19,9 +21,26 @@ const Home: React.FC = ({ children }) => {
 			setLoading(false);
 		};
 
-		exec();
+		const getStorageItem = (): ClimateDetails | void => {
+			const climateDetails = localStorage.getItem(climateDetailsToken);
+			if (climateDetails) {
+				setClimate(JSON.parse(climateDetails));
+			} else {
+				exec();
+			}
+		};
+		getStorageItem();
 		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, []);
+
+	React.useEffect(() => {
+		const storageDataFunction = (): void => {
+			if (climate) {
+				localStorage.setItem(climateDetailsToken, JSON.stringify(climate));
+			}
+		};
+		storageDataFunction();
+	}, [climate]);
 
 	if (!climate) {
 		return (
